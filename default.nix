5a72f702232594212f762e586324c775c9ef8fe2
Sam Craig <contact@samcraig.io>
2020-06-10 21:51:43 -0400

    style: removed inheritance scope

--- default.nix
+++ default.nix
@@ -1,14 +1,1 @@
-{ packages ? import ./packages {} }:
-{
-  inherit (packages)
-    go_1_14_2
-    go
-
-    skaffold_1_10_1
-    skaffold_1_10_0
-    skaffold
-
-    nodejs_10_19_0
-    nodejs
-    ;
-}
+import ./packges {}
