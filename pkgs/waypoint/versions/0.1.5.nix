2524feaee84302dc168e33ef47e13a0441a79a5a
Sam Craig <contact@samcraig.io>
2020-11-26 18:36:37 -0500

    fix: update stub derivation hash

--- pkgs/waypoint/versions/0.1.5.nix
+++ pkgs/waypoint/versions/0.1.5.nix
@@ -8,5 +8,5 @@ let
 in
   build {
     version = "0.1.5";
-    sha256 = "0cir9ld61pq1rziw6vyz1ihkq0aylzrsxg1vqb9544rwnbq7z2qs";
+    sha256 = "0bhkzkkb5mi7knc0vp08h1s1xwyzpbl0v4clmi9rswia3ha6m9yq";
   }
