39ff3a735d90604261c3a998eb4ea258f9c4a64f
Sam Craig <contact@samcraig.io>
2020-11-26 18:50:11 -0500

    style: organize package layout
    - add domain labels
    - alphabetize domains & packages

--- packages.nix
+++ packages.nix
@@ -13,28 +13,39 @@ rec {
     bazelSelf = bazel_3_2_0;
   };
 
-  mirror = mirror_1_0_2;
-  mirror_1_0_2 = package "mirror" "1.0.2" {};
+  # === go ===
 
   go = go_1_14_4;
-  go_1_14_4 = nixpkgs.pkgs.go_1_14; 
+  go_1_14_4 = nixpkgs.pkgs.go_1_14;
+
+  # === golangci-lint ===
 
   golangci-lint = golangci-lint_1_27_0;
   golangci-lint_1_27_0 = package "golangci-lint" "1.27.0" {};
+
+  # === google-cloud-sdk ===
 
   google-cloud-sdk = google-cloud-sdk_268_0_0;
   google-cloud-sdk-gce = google-cloud-sdk-gce_268_0_0;
   google-cloud-sdk_268_0_0 = package "google-cloud-sdk" "268.0.0" {};
   google-cloud-sdk-gce_268_0_0 = google-cloud-sdk.override { withGce = true; };
 
+  # === helm ===
+  
   helm = helm_3_2_1;
   helm_3_2_1 = package "helm" "3.2.1" {};
+  
+  # === jq ===
 
   jq = jq_1_6;
   jq_1_6 = package "jq" "1.6" {};
 
+  # === k9s ===
+
   k9s = k9s_0_19_4;
   k9s_0_19_4 = package "k9s" "0.19.4" {};
+
+  # === openjdk ===
 
   openjdk = openjdk_14;
   openjdk_headless = openjdk_14_headless;
@@ -45,12 +56,23 @@ rec {
   openjdk_8 = nixpkgs.pkgs.openjdk8;
   openjdk_8_headless = nixpkgs.pkgs.openjdk8_headless;
 
+  # === mirror ===
+  
+  mirror = mirror_1_0_2;
+  mirror_1_0_2 = package "mirror" "1.0.2" {};
+
+  # === nodejs ===
+  
   nodejs = nodejs_12_18_3;
   nodejs_12_18_3 = package "nodejs" "12.18.3" {};
   nodejs_10_19_0 = package "nodejs" "10.19.0" {};
 
+  # === python ===
+
   python = python_3_7_7;
   python_3_7_7 = nixpkgs.pkgs.python37;
+
+  # === rust ===
 
   cargo = cargo_1_43_0;
   cargo_1_43_0 = rust_1_45_2_packages.cargo;
@@ -66,11 +88,15 @@ rec {
   rustc = rustc_1_45_2;
   rustc_1_45_2 = rust_1_45_2_packages.rustc;
 
+  # === skaffold ===
+
   skaffold = skaffold_1_15_0;
   skaffold_1_15_0 = package "skaffold" "1.15.0" {};
   skaffold_1_11_0 = package "skaffold" "1.11.0" {};
   skaffold_1_10_1 = package "skaffold" "1.10.1" {};
   skaffold_1_10_0 = package "skaffold" "1.10.0" {};
+
+  # === waypoint ===
 
   waypoint = waypoint_0_1_5;
   waypoint_0_1_5 = package "waypoint" "0.1.5" {
